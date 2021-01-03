d20ca48804b6aaa047b7736d3291f2622154c2df
Mikael Kalms <mikael@kalms.org>
2021-01-03 23:54:44 +0100

    Moved shallow clone options to submodules

--- build_engine_linux.groovy
+++ build_engine_linux.groovy
@@ -9,21 +9,16 @@ pipelineJob('build_engine_linux') {
           }
           branch('*/main')
           extensions {
-            cloneOptions {
-              // Perform shallow clone, so that Git will not download history of the project.
-              shallow()
-              // Set shallow clone depth, so that git will only download recent history of the project,
-              //  saving time and disk space when you just want to access the latest version of a repository.
-              depth(1)
-              // Do not check out tegs
-              noTags()
-              
-            }
             submoduleOptions {
               // Use credentials from the default remote of the parent project
               parentCredentials()
               // Retrieves all submodules recursively.
               recursive()
+              // Perform shallow clone, so that Git will not download history of the project.
+              shallow()
+              // Set shallow clone depth, so that git will only download recent history of the project,
+              //  saving time and disk space when you just want to access the latest version of a repository.
+              depth(1)
             }
           }
         }
