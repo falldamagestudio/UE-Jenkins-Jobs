69d4613808e353d82c53df387069f21d872430a2
Mikael Kalms <mikael@kalms.org>
2021-01-03 16:53:57 +0100

    Authenticate with default github-user

--- build_engine_linux.groovy
+++ build_engine_linux.groovy
@@ -5,6 +5,7 @@ pipelineJob('build_engine_linux') {
         git {
           remote {
             url('https://github.com/Kalmalyzer/UE-Jenkins-Engine')
+            credentials('github-user')
           }
           branch('*/main')
         }
