3b7ddd76ced09e0ca22adb760bc9211d92750b39
Mikael Kalms <mikael@kalms.org>
2021-01-23 17:33:02 +0100

    Build windows engine from main branch please

--- build_engine_windows.groovy
+++ build_engine_windows.groovy
@@ -10,7 +10,7 @@ pipelineJob('build_engine_windows') {
             url('https://github.com/Kalmalyzer/UE-Jenkins-Engine')
             credentials('github-user')
           }
-          branch('*/windows-agents')
+          branch('*/main')
           extensions {
 
             // We use submoduleOption rather than submoduleOptions, since submoduleOption offers more configurability
