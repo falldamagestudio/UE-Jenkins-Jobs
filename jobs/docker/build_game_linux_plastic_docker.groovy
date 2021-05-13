3dc7fef8c243454a56f79d7fe1ffe7e1d743acb5
Mikael Kalms <mikael@kalms.org>
2021-05-13 22:38:11 +0200

    Added build_game_linux_plastic_docker

---
+++ jobs/docker/build_game_linux_plastic_docker.groovy
@@ -0,0 +1,29 @@
+pipelineJob('docker/build_game_linux_Plastic_docker') {
+
+  displayName("Build Game - Linux - Plastic - Docker")
+
+  properties {
+    disableConcurrentBuilds()
+  }
+
+  definition {
+    cpsScm {
+      scm {
+        plasticSCM {
+          // Which repository & branch to check out
+          // Reference: https://github.com/jenkinsci/plasticscm-plugin#selector-format
+          selector('''repository "UE-Jenkins-Game@ue_jenkins_buildsystem@cloud"
+                      path "/"
+                      smartbranch "/main"''')
+
+          // At start of checkout, undo any changed files in the workspace directory and then run a workspace update
+          // Reference: https://github.com/jenkinsci/plasticscm-plugin#freestyle-projects
+          cleanup('MINIMAL')
+        }
+      }
+      scriptPath('build_game_linux_plastic_docker.groovy')
+      // Obtain the Pipeline script contents directly from the SCM without performing a full checkout.
+      lightweight()
+    }
+  }
+}
