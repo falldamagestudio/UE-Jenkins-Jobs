5ed2f5cfc79be5bbe85b538dc730535a364249e7
Mikael Kalms <mikael@kalms.org>
2021-05-13 22:54:03 +0200

    Added build_game_win64_plastic_docker

---
+++ jobs/docker/build_game_win64_plastic_docker.groovy
@@ -0,0 +1,35 @@
+pipelineJob('docker/build_game_win64_plastic_docker') {
+
+  displayName("Build Game - Win64 - Plastic - Docker")
+
+  properties {
+    disableConcurrentBuilds()
+  }
+
+  definition {
+    cpsScm {
+      scm {
+        plasticSCM {
+          // Which repository & branch to check out
+          // Reference: https://github.com/jenkinsci/plasticscm-plugin#selector-format
+          selector('''repository "UE-Jenkins-Game@ue_jenkins_buildsystem@cloud"
+                      path "/"
+                      smartbranch "/main"''')
+
+          // At start of checkout, undo any changed files in the workspace directory and then run a workspace update
+          // Reference: https://github.com/jenkinsci/plasticscm-plugin#freestyle-projects
+          cleanup('MINIMAL')
+
+          // Perform single-workspace checkout
+          useMultipleWorkspaces(false)
+
+          // Place checkout result directly into Jenkins' workspace folder
+          directory('')
+        }
+      }
+      scriptPath('build_game_win64_plastic_docker.groovy')
+      // Obtain the Pipeline script contents directly from the SCM without performing a full checkout.
+      lightweight()
+    }
+  }
+}
